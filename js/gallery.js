30e543b909be5f6c4fb6f029437517b6d3368360
Vika <queenviktoriyphp@gmail.com>
2021-03-05 19:53:53 +0200

    начела загружения з сервера; только фотки, количество коменти и лайков

--- js/gallery.js
+++ js/gallery.js
@@ -27,48 +27,67 @@
       }
     });
 
-    bigPicture.querySelector(".likes-count").textContent =
-      pictureList[0].likes[
-        Math.floor(Math.random() * pictureList[0].likes.length)
-      ];
+    bigPicture.querySelector(".likes-count").textContent = photo.likes;
+    // pictureList[0].likes[
+    //   Math.floor(Math.random() * pictureList[0].likes.length)
+    // ];
 
-    bigPicture.querySelector(".comments-count").textContent =
-      pictureList[0].commentsQuantity[
-        Math.floor(Math.random() * pictureList[0].commentsQuantity.length)
-      ];
+    bigPicture.querySelector(".comments-count").textContent = photo.comments;
+    // pictureList[0].commentsQuantity[
+    //   Math.floor(Math.random() * pictureList[0].commentsQuantity.length)
+    // ];
     bigPicture.querySelector(".social__caption").textContent =
-      pictureList[0].description[
-        Math.floor(Math.random() * pictureList[0].description.length)
-      ];
+      photo.description;
+    // pictureList[0].description[
+    //   Math.floor(Math.random() * pictureList[0].description.length)
+    // ];
 
-    //   var image = photoElement.querySelector("img");
-    //   image.src = pictureList[0].url;
-    // for (var i = 0; i <= 25; i++) {
-    photoElement.querySelector("img").src = pictureList[0].url[i];
-    // }
+    //маленькі картинки
+    photoElement.querySelector("img").src = photo.url;
+    // pictureList[0].url[i];
 
     photoElement.querySelector(".picture__stat--likes").textContent =
-      pictureList[0].likes[
-        Math.floor(Math.random() * pictureList[0].likes.length)
-      ];
+      photo.likes;
+    // pictureList[0].likes[
+    //   Math.floor(Math.random() * pictureList[0].likes.length)
+    // ];
 
     photoElement.querySelector(".picture__stat--comments").textContent =
-      pictureList[0].commentsQuantity[
-        Math.floor(Math.random() * pictureList[0].commentsQuantity.length)
-      ];
-    // console.log(photoElement);
-    // var pictureImg = document.querySelectorAll(".picture__img");
+      photo.comments.length;
+    // pictureList[0].commentsQuantity[
+    //   Math.floor(Math.random() * pictureList[0].commentsQuantity.length)
+    // ];
 
     return photoElement;
   };
 
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < 26; i++) {
-    fragment.appendChild(renderPhotos(pictureList[i]));
-  }
-  pictures.appendChild(fragment);
+  // var fragment = document.createDocumentFragment();
+  // for (var i = 0; i < 26; i++) {
+  //   fragment.appendChild(renderPhotos(pictureList[i]));
+  // }
+  // pictures.appendChild(fragment);
 
-  // bigPictureImgSrc.src = pictureList[0].url[4];
-  console.log(pictureList[0].url[1]);
-  console.log(userPhoto);
+  //получаємо фотки
+  var successHandler = function (photos) {
+    var fragment = document.createDocumentFragment();
+    for (let i = 0; i < 25; i++) {
+      fragment.appendChild(renderPhotos(photos[i]));
+    }
+    pictures.appendChild(fragment);
+    // userDialog.querySelector(".setup-similar").classList.remove("hidden");
+  };
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement("div");
+    node.style =
+      "z-index: 100; margin: 0 auto; text-align: center; background-color: red;";
+    node.style.position = "absolute";
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = "30px";
+
+    node.textContent = errorMessage;
+
+    document.body.insertAdjacentElement("afterbegin", node);
+  };
+  window.load(successHandler, errorHandler);
 })();
