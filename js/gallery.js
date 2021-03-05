61cbc7daf28de23c1446cb4f30619dfc3c9b591e
Vika <queenviktoriyphp@gmail.com>
2021-03-05 22:26:57 +0200

    закончила передачу даних со сервира( коментарии под фото и аватарки)

--- js/gallery.js
+++ js/gallery.js
@@ -10,9 +10,36 @@
     // console.log(pictureImg);
     // photoElement = bigPicture;
     // for (let i = 0; i < pictureList[0].url.length; i++) {
+
+    //велика картинка
+    var socialComment = document.querySelectorAll(".social__comment");
+
+    var socialComments = document.querySelector(".social__comments");
+    var comentTemplate = socialComments
+      .querySelector(".social__comment")
+      .cloneNode(true);
+
     photoElement.addEventListener("click", function (evt) {
       bigPicture.classList.remove("hidden");
       bigPictureImgSrc.src = evt.target.src;
+      bigPicture.querySelector(".likes-count").textContent = photo.likes;
+      bigPicture.querySelector(".comments-count").textContent =
+        photo.comments.length;
+      bigPicture.querySelector(".social__caption").textContent =
+        photo.description;
+      socialComments.innerHTML = "";
+      for (let i = 0; i < photo.comments.length; i++) {
+        var newComment = comentTemplate.cloneNode(true);
+        socialComments.appendChild(newComment);
+        var socialPicture = newComment.querySelector(".social__picture");
+        socialPicture.src = photo.comments[i].avatar;
+        console.log(socialPicture);
+        var socialText = newComment.querySelector(".social__text");
+        socialText.textContent = photo.comments[i].message;
+      }
+
+      // socialPicture.querySelector(".social__picture").src =
+      //   photo.comments.avatar;
       console.log(evt.target);
     });
 
@@ -27,17 +54,19 @@
       }
     });
 
-    bigPicture.querySelector(".likes-count").textContent = photo.likes;
+    // bigPicture.querySelector(".likes-count").textContent = photo.likes;
+
     // pictureList[0].likes[
     //   Math.floor(Math.random() * pictureList[0].likes.length)
     // ];
 
-    bigPicture.querySelector(".comments-count").textContent = photo.comments;
+    // bigPicture.querySelector(".comments-count").textContent =
+    //   photo.comments.length;
     // pictureList[0].commentsQuantity[
     //   Math.floor(Math.random() * pictureList[0].commentsQuantity.length)
     // ];
-    bigPicture.querySelector(".social__caption").textContent =
-      photo.description;
+    // bigPicture.querySelector(".social__caption").textContent =
+    //   photo.description;
     // pictureList[0].description[
     //   Math.floor(Math.random() * pictureList[0].description.length)
     // ];
